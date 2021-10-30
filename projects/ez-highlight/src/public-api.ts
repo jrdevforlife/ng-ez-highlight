711b4821d47cd3da7d8acdcf62d2a7f3050173c5
jasonfritsche <fritsche.jason@gmail.com>
2021-10-29 21:49:32 -0500

    generated directive

---
+++ projects/ez-highlight/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ez-highlight
+ */
+
+export * from './lib/ez-highlight.service';
+export * from './lib/ez-highlight.component';
+export * from './lib/ez-highlight.module';
